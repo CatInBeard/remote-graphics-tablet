6b6883844365c99f207f92d5ffa4287b0994dfe2
Grigoriy Efimov <efimov-gm@newpage.xyz>
2025-01-10 19:52:19 +0300

    Improve CI building

--- CI/arch_build/build.sh
+++ CI/arch_build/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 docker build -t build-cpp:arch .
 docker run -v "$PWD/../../:/build/" --user build -w "/build/build/archlinux" build-cpp:arch make
--- CI/deb_build/build.sh
+++ CI/deb_build/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 docker build -t build-cpp:deb .
 docker run -v "$PWD/../../:/build/" --user build -w "/build/build/debian" build-cpp:deb make
