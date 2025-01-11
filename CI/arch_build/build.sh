dd40e12c0d840a43ef8e4b3ddd7c6fe9c792a930
Grigoriy Efimov <efimov-gm@newpage.xyz>
2025-01-11 23:33:46 +0300

    Fix tags in builds

--- CI/arch_build/build.sh
+++ CI/arch_build/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+git fetch origin main --tags
+
 docker build -t build-cpp:arch .
 docker run -v "$PWD/../../:/build/" --user build -w "/build/build/archlinux" build-cpp:arch make
--- CI/deb_build/build.sh
+++ CI/deb_build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-version=$(git describe --tags --always --abbrev=0 2>/dev/null | { read input; if [ -z "$input" ]; then echo "1.0.0"; else echo "$input"; fi; })
+
+git fetch origin main --tags
+version=$(git tag --list --sort=-creatordate | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -1 | { read input; if [ -z "$input" ]; then echo "1.0.0"; else echo "$input"; fi; })
 
 docker build -t build-cpp:deb .
 docker run -v "$PWD/../../:/build/" --user build -w "/build/build/debian" build-cpp:deb make version="$version"
